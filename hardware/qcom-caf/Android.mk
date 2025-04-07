66627e0115edee2089a9538f0d4a5bd412a2341b
Chattrapat Sangmanee <aomsin27@hotmail.co.th>
2025-04-07 18:53:41 +0700

    bring back qcom-caf

--- hardware/qcom-caf/Android.mk
+++ hardware/qcom-caf/Android.mk
@@ -1,7 +1,6 @@
 LOCAL_PATH := $(call my-dir)
-HAL_PLATFORM := msm8998
-HAL_PATH := hardware/qcom-caf/$(HAL_PLATFORM)
+HAL_PATH := $(LOCAL_PATH)
 
-include $(LOCAL_PATH)/display/Android.mk
+include $(HAL_PATH)/display/Android.mk
 include $(HAL_PATH)/audio/Android.mk
 include $(HAL_PATH)/media/Android.mk
