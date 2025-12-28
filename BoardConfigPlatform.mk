92cf357095f688ff33a9774d8849435acf06cb3b
Chattrapat Sangmanee <aomsin27@hotmail.co.th>
2025-12-29 04:44:48 +0700

    Fix build

--- BoardConfigPlatform.mk
+++ BoardConfigPlatform.mk
@@ -128,7 +128,7 @@ endif
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix_legacy.xml \
-    vendor/lineage/config/device_framework_matrix.xml \
+    hardware/lineage/interfaces/compatibility_matrices/compatibility_matrix.lineage.xml \
     $(PLATFORM_PATH)/device_framework_matrix.xml
 
 DEVICE_MATRIX_FILE += hardware/qcom-caf/common/compatibility_matrix.xml
